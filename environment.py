329ae6e79a5f3f11a8b364557fce52e28e30a377
agarpu <a.garrido.puerto@gmail.com>
2024-07-03 10:25:10 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,4 +1,3 @@
-# Creación de la red Deep Q-Learning
 # Creación del Entorno
 
 # Importar librerías
@@ -7,7 +6,7 @@ import numpy as np
 # Construir el entorno en una clase
 class Environment(object):
     # Introducir e inicializar los parametros y variables del entorno
-    def __init__(self,optimal_temperature=(18.9,24.0),initial_month = 0,initial_number_users = 10, initial_rate_data=60):
+    def __init__(self,optimal_temperature=(18.0,24.0),initial_month = 0,initial_number_users = 10, initial_rate_data=60):
         self.monthly_atmospheric_temperature = [1.0,5.0,7.0,10.0,11.0,20.0,23.0,24.0,22.0,10.0,5.0,1.0]
         self.initial_month = initial_month
         self.atmospheric_temperature = self.monthly_atmospheric_temperature[initial_month]
@@ -24,9 +23,30 @@ class Environment(object):
         self.current_number_users=initial_number_users
         self.initial_rate_data=initial_rate_data
         self.current_rate_data=initial_rate_data
+        self.intrinsec_temperature=self.atmospheric_temperature + 1.25*self.current_number_users + 1.25*self.current_rate_data
+        self.temperature_ai=self.intrinsec_temperature
+        self.temperature_no_ai=(self.optimal_temperature[0] + self.optimal_temperature[1])/2.0
+        self.total_energy_ai=0.0
+        self.total_energy_no_ai=0.0
+        self.reward=0.0
+        self.game_over=0 #será 1 cuando termine 
+        self.train=1 #modo entrenamiento=1 #modo simulación=0
         
     # Crear un método que actualice justo después de que la ia ejecute una acción
-    
+    def update_env(self, direction, energy_ai, month):
+        #OBTENCIÓN DE LA RECOMPENSA
+
+        #calcular la energia gastada por el sistema de refrigeración del server sin AI.
+
+        #calcular la recompensa
+
+        #escalar la recompensa
+
+        #OBTENCION DEL SIGUIENTE ESTADO
+
+        # actualizar la temperatura atmosferica
+
+        #OBTENCION DEL GAMEOVER
     
     # Crear un método que reinicie el entorno
     
